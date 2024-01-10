8e78c64052f7b6f730ccdc22f5795f26ed15a74b
Sandeep Maddheshiya <113048079+Itz-Sandy@users.noreply.github.com>
2024-01-10 23:40:12 +0530

    first commit

---
+++ 75 hard challange/Day 10 - Arrays/Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,16 @@
+
+public class Find_All_Numbers_Disappeared_in_an_Array {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int val : nums) {
+            set.add(val);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
---
+++ 75 hard challange/Day 10 - Arrays/Move_Zeroes.java
@@ -0,0 +1,27 @@
+
+
+public class Move_Zeroes {
+    public void moveZeroes(int[] nums) {
+        int m=-1;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]==0)
+            {
+                if(m==-1 || nums[m]!=0)
+                {
+                    m=i;
+                }
+            }
+            else
+            {
+                if(m!=-1)
+                {
+                    int temp = nums[i];
+                    nums[i]=nums[m];
+                    nums[m]=temp;
+                    m++;
+                }
+            }
+        }
+    }
+}
