ca84adcb5b8745add48cf03df5d411ab905f3a64
Sandeep Maddheshiya <113048079+Itz-Sandy@users.noreply.github.com>
2024-01-21 22:42:16 +0530

    first commit

---
+++ 75 hard challange/Day 21- linked List/Middle_of_LL.java
@@ -0,0 +1,13 @@
+
+
+public class Middle_of_LL {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow=slow.next;
+            fast =fast.next.next;
+        }
+        return slow;
+    }
+}
